56e3443cc8ce4ba2ab4970eaf78460af00dac03d
Kashaf Khan <=>
2020-12-04 19:39:19 +0500

    Our first upload

---
+++ code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main(){
+	char a[20];
+	printf("Enter Your name:");
+	fgets(a,20,stdin);
+	if(a=Hassaan ALi)
+	{
+		printf("Allowed in CLass")
+	}
+    else {
+    	printf("yOU aRE NOT ALLOWED")
+    }
+}
+
+	
